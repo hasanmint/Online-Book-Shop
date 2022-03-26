1590512a3abc2c9ff9425a009f344f572eabf1dd
hasanmint73 <hasanmint73@gmail.com>
2022-03-26 18:19:36 +0600

    All product show

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='display-4 mb-5'>Book Shop Product</h2>
+        </div>
+    );
+};
+
+export default Header;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
-    
-    return (
-        <div>
-            <p><img src={props.product.img} alt="" /></p>
-            <p>{props.product.name}</p>
-            <p>{props.product.price}</p>
-        </div>
-    );
+	const { name, img, price } = props.product;
+	return (
+		<div className="container">
+			<div className="row">
+                <div className="col-md-4 col-sm-12">
+                <div className="product">
+				<img src={img} alt="" />
+				<div className="product-info">
+					<h6 className="product-name">{name}</h6>
+					<p>Price: ${price}</p>
+				</div>
+				<button className="btn-cart">Add Button</button>
+			</div>
+                </div>
+            </div>
+		</div>
+	);
 };
 
 export default Product;
